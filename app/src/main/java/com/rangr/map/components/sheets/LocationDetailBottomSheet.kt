cdc662fe1def68b7ccbf4ee3d4df81df2c47a15d
Matt Chapman <matthew.chapman@xero.com>
2024-01-27 11:51:27 +1300

    005 Add share functionality

--- app/src/main/java/com/rangr/map/components/sheets/LocationDetailBottomSheet.kt
+++ app/src/main/java/com/rangr/map/components/sheets/LocationDetailBottomSheet.kt
@@ -1,21 +1,29 @@
 package com.rangr.map.components.sheets
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import com.rangr.map.MapViewModel
 import com.rangr.map.components.TextButton
 import com.rangr.map.models.GeoPosition
 import com.rangr.map.models.SheetType
+import com.rangr.ui.theme.RangrDark
+import com.rangr.util.ClipboardUtils
 
 @Composable
 fun LocationDetailBottomSheet(mapViewModel: MapViewModel) {
+    val ctx = LocalContext.current
     val tappedPoint = mapViewModel.tappedPoint.observeAsState(null)
 
     val tp = tappedPoint.value ?: return Text("Tapped point is null")
@@ -51,6 +59,19 @@ fun LocationDetailBottomSheet(mapViewModel: MapViewModel) {
         Text("LAT: ${gp.latLngDegreesMinutes.latitude}")
         Text("LNG: ${gp.latLngDegreesMinutes.longitude}")
         Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                ClipboardUtils.copyToClipboard(ctx, "Location Data", gp.toShareableString())
+                Toast.makeText(ctx, "Copied location to clipboard", Toast.LENGTH_SHORT).show()
+            },
+            colors = ButtonDefaults.buttonColors(
+                contentColor = RangrDark, backgroundColor = Color.Gray
+            ),
+        ) {
+            Text(text = "COPY TO CLIPBOARD")
+        }
 
         TextButton(text = "CREATE WAYPOINT", onClick = {
             mapViewModel.setBottomSheetType(SheetType.WaypointCreation)
